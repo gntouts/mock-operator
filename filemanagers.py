1b565e867a53b0effeb08fd9090efd9dd5d75ea9
Georgios Ntoutsos <gntouts@nubificus.co.uk>
2023-12-04 14:58:34 +0200

    Add timer for periodic checks
    Signed-off-by: Georgios Ntoutsos <gntouts@nubificus.co.uk>

--- filemanagers.py
+++ filemanagers.py
@@ -98,3 +98,38 @@ def delete_fn(body, **kwargs):
     targetDir = os.path.join(DEFAULT_PATH, namespace)
     if os.path.exists(targetDir):
         shutil.rmtree(targetDir)
+
+@kopf.timer('filemanagers', interval=10.0)
+def ensure_fn(spec, body, **kwargs):
+    name = body['metadata']['name']
+    namespace = body['metadata']['namespace']
+    desiredFileCount = int(spec['files'])
+    logging.info(
+        f'A timer handler is called for "{name}" in namespace "{namespace}" for {desiredFileCount} files.')
+    targetDir = os.path.join(DEFAULT_PATH, namespace)
+    if not os.path.exists(targetDir):
+        os.makedirs(targetDir)
+        logging.info(f"Directory '{targetDir}' created.")
+    else:
+        logging.info(f"Directory '{targetDir}' already exists.")
+
+    existing_files = os.listdir(targetDir)
+
+    if len(existing_files) == desiredFileCount:
+        logging.info("Resources already exist.")
+        return
+    if len(existing_files) < desiredFileCount:
+        logging.info("Creating extra files.")
+        amount = desiredFileCount - len(existing_files)
+        for i in range(amount):
+            temp = os.path.join(targetDir, generate_random_filename())
+            with open(temp, 'w') as file:
+                file.write(f'This is file {i}')
+        return
+    logging.info("Deleting extra files.")
+    amount = len(existing_files) - desiredFileCount
+    for i in range(amount):
+        temp = os.path.join(targetDir, existing_files[i])
+        os.remove(temp)
+        logging.info
+    return
